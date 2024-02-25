684d0fa72cae7b23680b49f8fb0d88993a9fdf45
Daksh0786 <Dakshkhandelwal786@gmail.com>
2024-02-25 21:37:32 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider,Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
+import Home from './Pages/Home'
+
+const router = createBrowserRouter([
+{
+  path:'/',
+  element:<Login/>
+
+}
+])
 
 function App() {
   return (
-    <div className="App">
-      <Route>
-        <Switch>
-          <Router exact path = "/">
-
-          </Router>
-        </Switch>
-      </Route>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import classes from "./Login.module.css"; // Corrected import statement
 
-const Login = (props)=> {
-
-    return (
+const Login = (props) => {
+  return (
+    <div>
+      <nav className={classes.ima}>
+        <a href=" ">
+          <img src="/Images/login-logo.svg" alt=" "></img>
+        </a>
         <div>
-            Login
+          
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 export default Login;
